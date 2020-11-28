48b468d80b9976e3c26d92ab2241efe1ea167fa1
RedGhoul <avaneesab5@gmail.com>
2020-11-28 09:14:10 -0500

    added html

--- JobGetter/settings.py
+++ JobGetter/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 import mimetypes 
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/html", ".html", True)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
